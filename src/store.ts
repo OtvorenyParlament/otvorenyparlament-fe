48448b1009a5aa63494ab25dcb9a6e434c1cffe2
Jozef Sukovsky <sukovskyj@gmail.com>
2018-10-07 22:11:28 +0200

    fixed variables, added currentPeriodNum to store

--- src/store.ts
+++ src/store.ts
@@ -5,10 +5,12 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    currentPeriodNum: process.env.VUE_APP_DEFAULT_PERIOD,
   },
   mutations: {
-
+    newPeriod(state, newPeriodNum) {
+      state.currentPeriodNum = newPeriodNum;
+    },
   },
   actions: {
 

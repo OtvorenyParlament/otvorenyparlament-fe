88db1c89eca65e06cda4a49a7ea9db6f0a1a272c
Jozef Sukovsky <sukovskyj@gmail.com>
2018-11-22 13:42:57 +0100

    VotinDetail and VotingQuery changes

--- src/views/voting/VotingDetailView.vue
+++ src/views/voting/VotingDetailView.vue
@@ -1,12 +1,23 @@
 <template>
     <b-container v-if="voting">
         <h1>{{ voting.topic }}</h1>
-        <b-badge variant="info">Hlasovanie</b-badge>
+        <b-badge :variant="resultVariant(voting.result)">{{ VotingResult[voting.result] }}</b-badge>
         <b-row>
             <b-col>{{ formatDate(voting.timestamp) }}</b-col>
-            <b-col>{{ voting.session }}</b-col>
-            <b-col>{{ voting.press }}</b-col>
-            <b-col>{{ VotingResult[voting.result] }}</b-col>
+            <b-col>Číslo schôdze: {{ voting.session.sessionNum }}</b-col>
+            <b-col>Číslo hlasovania: {{ voting.votingNum }}</b-col>
+            <b-col><div v-if="voting.press">Číslo tlače: {{ voting.press.pressNum }}</div></b-col>
+            <b-col><a :href="voting.url" target="_blank">Otvoriť na NRSR.sk</a></b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <votingPie :pieLabels="voting.chartSeries.labels" :pieSeries="voting.chartSeries.series"/>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-alert variant="secondary" show>V tomto zobrazení pripravujeme super-trooper funkcionalitu. Navštívte nás čoskoro</b-alert>
+          </b-col>
         </b-row>
     </b-container>
 </template>
@@ -21,6 +32,19 @@ export default {
       return {
           VotingResult: VotingResult,
       };
+  },
+  methods: {
+    resultVariant(resultValue) {
+      if (resultValue === 'A_0') {
+        return 'success';
+      } else if (resultValue === 'A_1') {
+        return 'danger';
+      } else if (resultValue === 'A_2') {
+        return 'warning';
+      } else {
+        return 'dark';
+      }
+    },
   },
   apollo: {
     voting: {

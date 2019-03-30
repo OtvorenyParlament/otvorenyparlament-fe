fb901dfcbba4f32fc6798e440d5652f35a97a0a5
Jozef Sukovsky <sukovskyj@gmail.com>
2019-03-30 21:15:20 +0100

    backgroundColorPalette changes

--- src/mixins/ChartPaletteMixin.js
+++ src/mixins/ChartPaletteMixin.js
@@ -2,15 +2,15 @@ export default {
     data: function () {
       return {
         backgroundColorPalette: [
-          '#377eb8',
-          '#4daf4a',
-          '#984ea3',
-          '#ff7f00',
-          '#e41a1c',
-          '#a65628',
-          '#f781bf',
-          '#999999',
-          '#ffff33',
+          '#006bff',
+          '#1eda45',
+          '#fd3545',
+          '#8000b3',
+          '#ffa700',
+          '#6c757d',
+          '#ff8eb2',
+          '#00acc1',
+          '#e4ff0f'
         ],
       }
     },

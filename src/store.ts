ca3552214bd84e45e7f85cc333ba09ed9589fbc0
Jozef Sukovsky <sukovskyj@gmail.com>
2019-07-26 15:14:55 +0200

    upgraded packages

--- src/store.ts
+++ src/store.ts
@@ -5,7 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    currentPeriodNum: parseInt(process.env.VUE_APP_DEFAULT_PERIOD, 10),
+    currentPeriodNum: parseInt((process.env.VUE_APP_DEFAULT_PERIOD || ''), 10),
     currentSessionId: null,
   },
   mutations: {
